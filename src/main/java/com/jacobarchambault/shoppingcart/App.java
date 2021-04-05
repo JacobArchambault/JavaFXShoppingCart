74e1c7372a43e05e0c4a7fe43dda463dbef4f7c3
Jacob Archambault <jacob.archambault@waystar.com>
2021-04-05 09:49:48 -0400

    Add listview to app.

--- src/main/java/com/jacobarchambault/shoppingcart/App.java
+++ src/main/java/com/jacobarchambault/shoppingcart/App.java
@@ -3,6 +3,7 @@ package com.jacobarchambault.shoppingcart;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,7 +14,7 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage stage) {
-		stage.setScene(new Scene(new VBox(10, new Label("Pick a book"))));
+		stage.setScene(new Scene(new VBox(10, new Label("Pick a book"), new ListView<String>())));
 		stage.show();
 	}
 
