13b5b24a28224fe7bbf623600d81a45cb8b072e5
Jacob Archambault <jacob.archambault@waystar.com>
2021-04-05 12:15:17 -0400

    Get arrayList elements from createFile static method.

--- src/main/java/com/jacobarchambault/shoppingcart/App.java
+++ src/main/java/com/jacobarchambault/shoppingcart/App.java
@@ -1,6 +1,12 @@
 package com.jacobarchambault.shoppingcart;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -14,16 +20,27 @@ import javafx.stage.Stage;
  * JavaFX App
  */
 public class App extends Application {
+	static List<String> createFile() throws IOException {
+		ArrayList<String> result = new ArrayList<>();
+
+		try (BufferedReader br = new BufferedReader(new FileReader("BookPrices.txt"))) {
+			while (br.ready()) {
+				result.add(br.readLine());
+			}
+		}
+		return result;
+	}
+
 	@Override
-	public void start(Stage stage) {
+	public void start(Stage stage) throws IOException {
 		stage
 				.setScene(
 						new Scene(
 								new VBox(
 										10,
 										new Label("Pick a book"),
-										new ListView<String>(FXCollections.observableArrayList("Some text", "Some more text", "Another text")))));
-		
+										new ListView<String>(FXCollections.observableArrayList(createFile())))));
+
 		stage.show();
 	}
 
